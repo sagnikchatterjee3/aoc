069fb587d8c0ca0544b7999f1bdc79f061a63380
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-22 14:33:31 +0530

    src: solved day2

--- aoc2022/day2/part1.ts
+++ aoc2022/day2/part1.ts
@@ -1,19 +1,35 @@
-type Guide = {
-    X: string;
-    Y: string;
-}
-
 const main = async () => {
-    const text = await (await Deno.readTextFile('./input.txt')).split("\n");
+  const data = await (await Deno.readTextFile("./input.txt")).split("\n");
+  let score = 0;
+  for (const line of data) {
+    const [x, y] = line.split(" ");
+    if (x === "A") {
+      if (y === "X") {
+        score += 4;
+      } else if (y === "Y") {
+        score += 8;
+      } else if (y === "Z") {
+        score += 3;
+      }
+    } else if (x === "B") {
+      if (y === "X") {
+        score += 1;
+      } else if (y === "Y") {
+        score += 5;
+      } else if (y == "Z") {
+        score += 9;
+      }
+    } else if (x === "C") {
+      if (y === "X") {
+        score += 7;
+      } else if (y === "Y") {
+        score += 2;
+      } else if (y === "Z") {
+        score += 6;
+      }
+    }
+  }
+  console.log(score);
+};
 
-    text.forEach((guide) => {
-        
-    })
-}
-
-try {
-    main();
-}
-catch (e) {
-    console.error(e);
-}
+await main();
