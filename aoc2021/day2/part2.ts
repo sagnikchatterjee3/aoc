ae5ceb9d2ab5fba2a9dcb3767641ffcd461eeb0b
sagnik.chatterjee3 <sagnik.chatterjee3@ibm.com>
2023-05-31 14:12:00 +0530

    src: added solutions

---
+++ aoc2021/day2/part2.ts
@@ -0,0 +1,8 @@
+const main = async () => {
+    const line = await(await (Deno.readTextFile('./input.txt'))).split('\n');
+    for(const item of line){
+        
+    }
+}
+
+await main();
--- aoc2022/day3/part1.ts
+++ aoc2022/day3/part1.ts
@@ -1,28 +1,31 @@
 
-const getPriority = (char: string) => {
-  const charCode = char.charCodeAt(0);
-  return charCode > 90 ? charCode - 96 : charCode - 38;
-};
+function getCommon(arr1: string, arr2: string): string {
+  // o(n^2) solution; can be optmized using hashmap
+  const result: string[] = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        result.push(arr1[i]);
+      }
+    }
+  }
+  return result.join('');
+}
 
 const main = async () => {
-  const data = await ((await Deno.readTextFile("./input.txt"))).split("\n");
-  let s = 0;
-  for (const item of data) {
-    const half = Math.floor(item.length / 2);
-    const [left, right] = [item.slice(0, half), item.slice(half)];
-
-    let found = undefined;
-
-    for (const char of left) {
-      if (!right.includes(char)) continue;
-      found = char;
-      break;
+  const data = await (await (Deno.readTextFileSync('./partial.txt')).split('\n'));
+  let result = 0;
+  data.forEach((item: string) => {
+    const [arr1, arr2] = item.slice(item.length / 2, item.length);
+    const common = getCommon(arr1, arr2);
+    if (common === common.toUpperCase()) {
+      result += common.charCodeAt(0) - 'A'.charCodeAt(0) + 27;
     }
-
-    if (!found) continue;
-    s += getPriority(found);
-  }
-  console.log(s);
-};
+    else {
+      result += common.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    }
+  });
+  console.log(result);
+}
 
 await main();
